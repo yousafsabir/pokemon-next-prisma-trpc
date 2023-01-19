45f5a5b0b878e03f8d5573686c2d9eb4500f9e5a
yousafsabir <myousafdev@gmail.com>
2023-01-18 17:59:31 -0800

    tested trpc server

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@styles/globals.css";
-import type { AppProps } from "next/app";
-
-export default function App({ Component, pageProps }: AppProps) {
+import type { AppType } from "next/app";
+import { trpc } from "@utils/trpc";
+const MyApp: AppType = ({ Component, pageProps }) => {
     return <Component {...pageProps} />;
-}
+};
+export default trpc.withTRPC(MyApp);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,42 +1,74 @@
 import Head from "next/head";
 import { Inter, Open_Sans } from "@next/font/google";
+import { trpc } from "@utils/trpc";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-    return (
-        <>
-            <Head>
-                <title>Roundest Pokemon | Home page</title>
-                <meta name="description" content="roundest pokemon" />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1"
-                />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <main className={"min-h-screen w-full grid place-content-center"}>
-                <div className="space-y-6 p-8">
+    const { data, isLoading, isError, isLoadingError } = trpc.hello.useQuery({
+        text: "Yousaf",
+    });
+    if (isLoading) {
+        return (
+            <>
+                <Head>
+                    <title>Roundest Pokemon | Home page</title>
+                    <meta name="description" content="roundest pokemon" />
+                    <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1"
+                    />
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
+                <main
+                    className={"min-h-screen w-full grid place-content-center"}
+                >
                     <h1 className="font-bold text-4xl text-center">
-                        Which Pokemon is the roundest
+                        loading....
                     </h1>
-                    <div className="border rounded p-8">
-                        <div className="flex space-x-5 items-center">
-                            <img
-                                src="https://placeimg.com/800/200/arch"
-                                alt=""
-                                className="rounded-lg object-cover object-center w-56 h-56 cursor-pointer"
-                            />
-                            <span>Vs</span>
-                            <img
-                                src="https://placeimg.com/800/200/arch"
-                                alt=""
-                                className="rounded-lg object-cover object-center w-56 h-56 cursor-pointer"
-                            />
+                </main>
+            </>
+        );
+    } else {
+        return (
+            <>
+                <Head>
+                    <title>Roundest Pokemon | Home page</title>
+                    <meta name="description" content="roundest pokemon" />
+                    <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1"
+                    />
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
+                <main
+                    className={"min-h-screen w-full grid place-content-center"}
+                >
+                    <div className="space-y-6 p-8">
+                        <h1 className="font-bold text-4xl text-center">
+                            {data?.greeting}
+                        </h1>
+                        <h1 className="font-bold text-4xl text-center">
+                            Which Pokemon is the roundest
+                        </h1>
+                        <div className="border rounded p-8">
+                            <div className="flex space-x-5 items-center">
+                                <img
+                                    src="https://placeimg.com/800/200/arch"
+                                    alt=""
+                                    className="rounded-lg object-cover object-center w-56 h-56 cursor-pointer"
+                                />
+                                <span>Vs</span>
+                                <img
+                                    src="https://placeimg.com/800/200/arch"
+                                    alt=""
+                                    className="rounded-lg object-cover object-center w-56 h-56 cursor-pointer"
+                                />
+                            </div>
                         </div>
                     </div>
-                </div>
-            </main>
-        </>
-    );
+                </main>
+            </>
+        );
+    }
 }
