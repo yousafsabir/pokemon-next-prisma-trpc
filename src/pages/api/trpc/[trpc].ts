914c8964e19c5450fc3ccec1b58da80d51640f93
yousafsabir <myousafdev@gmail.com>
2023-02-20 10:51:18 +0500

    Added Utility to get response type of a query from server and use it in frontend

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,8 +1,19 @@
 import * as trpcNext from "@trpc/server/adapters/next";
-import { appRouter } from "@server/routers/index";
+import { AppRouter, appRouter } from "@server/routers/index";
+import { inferProcedureOutput } from "@trpc/server";
 // export API handler
 // @see https://trpc.io/docs/api-handler
 export default trpcNext.createNextApiHandler({
     router: appRouter,
     createContext: () => ({}),
 });
+
+// Utility to get response type of a query and use it in frontend
+export type inferQueryResponse<
+    TrouteKey extends keyof AppRouter["_def"]
+> = inferProcedureOutput<AppRouter["_def"][TrouteKey]>;
+
+// type declaration with trpc v9
+// export type inferQueryResponse<
+//     TrouteKey extends keyof AppRouter["_def"]["queries"]
+// > = inferProcedureOutput<AppRouter["_def"]["queries"][TrouteKey]>;
